45d5ce93bae5946d58b92cc017c2ff84dc9b7e51
pechavvp <pechathebest@yandex.ru>
2021-03-22 23:21:18 +0300

    final

--- script.js
+++ script.js
@@ -60,6 +60,21 @@ function createMouse() {
 
 createMouse();
 
+let direction = 'right';
+steps = false;
+
+let input = document.createElement('input');
+document.body.appendChild(input);
+input.style.cssText = `
+margin: auto;
+margin-top: 40px;
+font-size: 30px;
+display: block;
+`;
+
+let score = 0;
+input.value = `Ваши очки: ${score}`;
+
 
 function move() {
     let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
@@ -67,10 +82,49 @@ function move() {
     snakeBody[snakeBody.length - 1].classList.remove('snakeBody');
     snakeBody.pop();
 
-    if (snakeCoordinates[0] < 10) {
-        snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0]+1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-    } else {
-        snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+    if (direction == 'right') {
+        if (snakeCoordinates[0] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0]+1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    } else if (direction == 'left') {
+        if (snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0]-1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    } else if (direction == 'up') {
+        if (snakeCoordinates[1] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1]+1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
+        }
+    } else if (direction == 'down') {
+        if (snakeCoordinates[1] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1]-1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY ="10"]'));
+        }
+    }
+
+    if (snakeBody[0].getAttribute('posX') == mouse.getAttribute('posX') && snakeBody[0].getAttribute('posY') == mouse.getAttribute('posY')) {
+        mouse.classList.remove('mouse');
+
+        let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
+        snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        createMouse();
+        score++;
+        input.value = `Ваши очки: ${score}`;
+    }
+
+    if (snakeBody[0].classList.contains('snakeBody')) {
+        setTimeout(()=> {
+            alert(`Игра окончена! Ваши очки: ${score}`);
+        }, 200);
+        clearInterval(interval);
+        snakeBody[0].style.background = 'url(gameover.jpg)';
     }
 
 
@@ -78,6 +132,30 @@ function move() {
     for (i = 0; i < snakeBody.length; i++) {
         snakeBody[i].classList.add('snakeBody');
     }
+
+    steps = true;
 }
 
 let interval = setInterval(move, 300);
+
+window.addEventListener('keydown', function(e) {
+    if (steps == true) {
+        if (e.key == "ArrowLeft" && direction != 'right') {
+            e.preventDefault();
+            direction = 'left';
+            steps = false;
+        } else if (e.key == "ArrowRight" && direction != 'left') {
+            e.preventDefault();
+            direction = 'right';
+            steps = false;
+        } else if (e.key == "ArrowUp" && direction != 'down') {
+            e.preventDefault();
+            direction = 'up';
+            steps = false;
+        } else if (e.key == "ArrowDown" && direction != 'up') {
+            e.preventDefault();
+            direction = 'down';
+            steps = false;
+        }
+    }
+});
